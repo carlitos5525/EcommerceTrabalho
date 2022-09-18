163ae7095a8602af11797a0c2d09c8267d4ff7a8
Carlos Mayer <carlitos5525.cap@gmail.com>
2022-09-18 16:14:54 -0300

    Create Database

---
+++ Migrations/20220918191402_initial_migration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcommerceTrabalho.Migrations
+{
+    public partial class initial_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Preco = table.Column<float>(type: "REAL", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true),
+                    IdCategoriaProduto = table.Column<int>(type: "INTEGER", nullable: false),
+                    CategoriaProdutoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_CategoriaProdutoId",
+                        column: x => x.CategoriaProdutoId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CategoriaProdutoId",
+                table: "Produtos",
+                column: "CategoriaProdutoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
