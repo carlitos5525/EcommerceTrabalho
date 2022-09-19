0ed16e94bbcb575dd38ff4c5e1ce785cb2855be4
Eduardo Nesi <eduardonesic@gmail.com>
2022-09-19 11:09:40 -0300

    create CategoriaController

---
+++ Controllers/CategoriaProdutoController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using FolhaPagamento.Models.Funcionario;
+
+namespace EcommerceTrabalho.Controllers
+{
+    [Route("api/categoria")]
+    [ApiController]
+    public class CategoriaProdutoController : ControllerBase
+    {
+
+
+    }
+
+
+}
