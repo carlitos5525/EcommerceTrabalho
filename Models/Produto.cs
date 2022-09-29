9b5784f53adeae660200933a6184fda68617b642
Deborah <deborahalynne01@gmail.com>
2022-09-29 19:50:42 -0300

    Validacoes

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EcommerceTrabalho.Models
 {
@@ -6,8 +7,18 @@ namespace EcommerceTrabalho.Models
     {
         public int Id {get; set;}
         public String Nome {get; set;}
+        [Required(ErrorMessage = "O campo nome é obrigatório.")]
         public float Preco {get; set;}
+         [Range(
+            0,
+            999,
+            ErrorMessage = "O valor do produto deve ser até R$999."
+        )]
         public String Descricao {get; set;}
+         [StringLength(
+            100,
+            ErrorMessage = "O campo de descrição deve conter no máximo 100 caracteres!"
+        )]
         public int CategoriaProdutoId {get; set;}
         public CategoriaProduto CategoriaProduto {get; set;}
     }
